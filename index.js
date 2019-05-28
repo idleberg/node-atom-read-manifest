61d5d0cb1d64aa4a5d4904198417ae1d6feb0495
idleberg <git@idleberg.com>
2019-05-28 21:52:02 +0200

    fixes

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 'use strict';
 
-const atom = require('atom');
 const findUp = require('find-up');
 const { promisify } = require('util');
 const { readFile, readFileSync } = require('fs');
-const { resolve } = require('path');
+const { join, resolve } = require('path');
 
 const readFileAsync = promisify(readFile);
 
-module.exports = (packageName = false) => {
+module.exports = async (packageName = '') => {
     let filePath;
 
     if (packageName) {
         const packagePath = atom.packages.resolvePackagePath(packageName);
         filePath = resolve(packagePath, 'package.json');
     } else {
-        filePath = findUp('package.json');
+        filePath = await findUp('package.json', { cwd: join(__dirname, '..') });
     }
 
     return readFileAsync(filePath, 'utf8').then(file => JSON.parse(file));
 };
 
-module.exports.sync = (packageName = false) => {
+module.exports.sync = (packageName = '') => {
     let filePath;
 
     if (packageName) {
         const packagePath = atom.packages.resolvePackagePath(packageName);
         filePath = resolve(packagePath, 'package.json');
     } else {
-        filePath = findUp.sync('package.json');
+        filePath = findUp.sync('package.json', { cwd: join(__dirname, '..') });
     }
-    const file = readFileSync(filePath, 'utf8');
 
-    return JSON.parse(file);
+    return JSON.parse(readFileSync(filePath, 'utf8'));
 };
