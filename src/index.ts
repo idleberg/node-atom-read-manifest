6fe29d1cd567eb4217b502e647593958be998db5
idleberg <git@idleberg.com>
2021-03-07 23:10:35 +0100

    fix linter warnings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { promises as fs, readFileSync } from 'fs';
 import { resolve, sep } from 'path';
-import callerCallsite = require('caller-callsite');
-import readPkgUp = require('read-pkg-up');
+import callerCallsite from 'caller-callsite';
+import readPkgUp from 'read-pkg-up';
 
-async function readManifest(packageName: string = ''): Promise<Object> {
+async function readManifest(packageName = ''): Promise<unknown> {
   const filePath: string = await resolvePackagePath(packageName);
 
   try {
@@ -14,7 +14,7 @@ async function readManifest(packageName: string = ''): Promise<Object> {
   }
 }
 
-function readManifestSync (packageName: string = ''): Object {
+function readManifestSync (packageName = ''): unknown {
   const filePath: string = resolvePackagePathSync(packageName);
 
   try {
@@ -30,15 +30,17 @@ async function resolvePackagePath(packageName: string): Promise<string> {
     ? packageName
     : getPackageName();
 
-  const packagePath: string | null = atom.packages.resolvePackagePath(packageName);
+  const packagePath: string | undefined = atom.packages.resolvePackagePath(packageName);
 
   if (packagePath) {
     return resolve(packagePath, 'package.json');
   }
 
-  const { packageJson } = await readPkgUp({cwd: __dirname});
+  const manifest = await readPkgUp({cwd: __dirname});
 
-  return await resolvePackagePath(packageJson.name);
+  return manifest?.packageJson
+    ? await resolvePackagePath(manifest.packageJson.name)
+    : '';
 }
 
 function resolvePackagePathSync(packageName: string): string {
@@ -46,26 +48,28 @@ function resolvePackagePathSync(packageName: string): string {
     ? packageName
     : getPackageName();
 
-  const packagePath: string | null = atom.packages.resolvePackagePath(packageName);
+  const packagePath: string | undefined = atom.packages.resolvePackagePath(packageName);
 
   if (packagePath) {
     return resolve(packagePath, 'package.json');
   }
 
-  const { packageJson } = readPkgUp.sync({cwd: __dirname});
+  const manifest = readPkgUp.sync({cwd: __dirname});
 
-  return resolvePackagePathSync(packageJson.name);
+  return manifest?.packageJson
+    ? resolvePackagePathSync(manifest.packageJson.name)
+    : '';
 }
 
 function getPackageName(): string {
-  const callerPath: string = callerCallsite().getFileName();
+  const callerPath: string | null | undefined = callerCallsite()?.getFileName();
   const packageDirPaths: string[] = atom.packages.getPackageDirPaths();
 
   const intersection: string[] = packageDirPaths.filter(packageDirPath => {
-    return callerPath.startsWith(packageDirPath);
+    return callerPath?.startsWith(packageDirPath);
   });
 
-  if (intersection?.length) {
+  if (callerPath && intersection?.length) {
     return callerPath
       .replace(intersection[0], '')
       .split(sep)
